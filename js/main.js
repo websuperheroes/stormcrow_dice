2185d2688f269f429a0670d37302066767f48142
Dave <davidajberner@gmail.com>
2015-03-11 06:09:06 +1100

    Updates to dice roller

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (function() {
     var Dice = {
         init: function() {
+            // this.dice_list = $('#dice_list > li');
             this.spinner = $('#spinner');
             this.die = $('#die');
             this.roll_result = $('#roll_result');
@@ -9,6 +10,7 @@
         dice_sides: 6,
         bind_events: function() {
             this.die.on('click', $.proxy(this.roll, this));
+            // this.dice_list.on('click', $.proxy(this.pick_sides, this));
        },
         timeOutLength: 500,
 
@@ -21,13 +23,17 @@
             setTimeout(function(){
 
               self.spinner.hide();
-              self.roll_result.html(res);
+              self.roll_result.attr('class', 'die');
+              self.roll_result.addClass('d' + self.dice_sides + '-' + res);
 
             }, this.timeOutLength);
         },
         pick_sides: function(event){
-
-            this.dice_sides = 8;
+            // $("#dice_list > li").removeClass("is-active");
+            // this.dice_list.addClass('is-active');
+            // var sides = this.dice_list.attr('id');
+            // var sides_string = sides.substring(1, sides.length);
+            // this.dice_sides = sides_string;
         }
     };
 
@@ -39,6 +45,7 @@
         var sides = $(this).attr('id');
         var sides_string = sides.substring(1, sides.length);
         Dice.dice_sides = sides_string;
+        Dice.roll();
     });
 
 })();
